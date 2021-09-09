a9c17b91f9591d15eec96df7521f6053c1367f3a
kedaitayar <kedaitayar09@gmail.com>
2021-09-09 20:27:59 +0800

    fix: move backup and restore function to setting page

--- app/src/main/java/io/github/kedaitayar/mfm/ui/dashboard/account/main_account/AccountDashboardFragment.kt
+++ app/src/main/java/io/github/kedaitayar/mfm/ui/dashboard/account/main_account/AccountDashboardFragment.kt
@@ -1,27 +1,18 @@
 package io.github.kedaitayar.mfm.ui.dashboard.account.main_account
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.PopupMenu
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
-import com.ebner.roomdatabasebackup.core.RoomBackup
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
-import io.github.kedaitayar.mfm.MainActivity
 import io.github.kedaitayar.mfm.R
-import io.github.kedaitayar.mfm.data.database.MFMDatabase
 import io.github.kedaitayar.mfm.databinding.FragmentAccountDashboardBinding
 import io.github.kedaitayar.mfm.ui.main.MainFragmentDirections
-import io.github.kedaitayar.mfm.ui.main.MainViewModel
 import io.github.kedaitayar.mfm.util.exhaustive
 import io.github.kedaitayar.mfm.util.notNull
 import kotlinx.coroutines.flow.collect
@@ -34,7 +25,6 @@ private const val TAG = "AccountDashboardFragmen"
 @AndroidEntryPoint
 class AccountDashboardFragment : Fragment(R.layout.fragment_account_dashboard) {
     private val accountDashboardViewModel: AccountDashboardViewModel by viewModels()
-    private val mainViewModel: MainViewModel by activityViewModels()
     private val binding: FragmentAccountDashboardBinding by viewBinding()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -42,84 +32,9 @@ class AccountDashboardFragment : Fragment(R.layout.fragment_account_dashboard) {
         childFragmentManager.beginTransaction().replace(R.id.account_list_fragment_container, AccountListFragment())
             .commit()
 
-        setupSettingMenu()
         setupDashboardInfo()
         setupAddAccountButtonListener()
         setupEventListener()
-    }
-
-    private fun setupSettingMenu() {
-        binding.buttonMore.setOnClickListener { buttonView ->
-            val popupMenu = PopupMenu(requireContext(), buttonView)
-            popupMenu.menuInflater.inflate(R.menu.menu_setting, popupMenu.menu)
-
-            popupMenu.setOnMenuItemClickListener { menuItem ->
-                when (menuItem.itemId) {
-                    R.id.backup -> {
-                        MaterialAlertDialogBuilder(requireContext())
-                            .setTitle("Backup")
-                            .setMessage(
-                                "The backup file will be store in `/storage/emulated/0/Android/data/io.github.kedaitayar.mfm/files/backup/mfm_db-[DateTime].sqlite3`. " +
-                                        "Move the file somewhere else because the file is deleted by Android after uninstalling the App." +
-                                        " After the backup is done, the app will be restarted."
-                            )
-                            .setNegativeButton("Cancel") { dialog, _ ->
-                                dialog.cancel()
-                            }
-                            .setPositiveButton("Backup") { _, _ ->
-                                backup()
-                            }
-                            .show()
-                        true
-                    }
-                    R.id.restore -> {
-                        restore()
-                        true
-                    }
-                    else -> true
-                }
-            }
-
-            popupMenu.show()
-        }
-    }
-
-    private fun backup() {
-        RoomBackup()
-            .context(requireContext())
-            .database(MFMDatabase.getDatabase(requireContext()))
-            .enableLogDebug(false)
-            .backupIsEncrypted(false)
-            .useExternalStorage(true)
-            .apply {
-                onCompleteListener { success, message ->
-                    if (success) {
-                        restartApp(Intent(requireContext(), MainActivity::class.java))
-                    } else {
-                        mainViewModel.showSnackbar("There an error. Error message: $message", Snackbar.LENGTH_SHORT)
-                    }
-                }
-            }
-            .backup()
-    }
-
-    private fun restore() {
-        RoomBackup()
-            .context(requireContext())
-            .database(MFMDatabase.getDatabase(requireContext()))
-            .enableLogDebug(false)
-            .backupIsEncrypted(false)
-            .useExternalStorage(true)
-            .apply {
-                onCompleteListener { success, message ->
-                    if (success) {
-                        restartApp(Intent(requireContext(), MainActivity::class.java))
-                    } else {
-                        mainViewModel.showSnackbar("There an error. Error message: $message", Snackbar.LENGTH_SHORT)
-                    }
-                }
-            }
-            .restore()
     }
 
     private fun setupEventListener() {
--- app/src/main/java/io/github/kedaitayar/mfm/ui/setting/SettingFragment.kt
+++ app/src/main/java/io/github/kedaitayar/mfm/ui/setting/SettingFragment.kt
@@ -1,8 +1,84 @@
 package io.github.kedaitayar.mfm.ui.setting
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.ebner.roomdatabasebackup.core.RoomBackup
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
+import io.github.kedaitayar.mfm.MainActivity
 import io.github.kedaitayar.mfm.R
+import io.github.kedaitayar.mfm.data.database.MFMDatabase
+import io.github.kedaitayar.mfm.databinding.FragmentSettingBinding
+import io.github.kedaitayar.mfm.ui.main.MainViewModel
+import timber.log.Timber
 
 class SettingFragment : Fragment(R.layout.fragment_setting) {
+    private val mainViewModel: MainViewModel by activityViewModels()
+    val binding: FragmentSettingBinding by viewBinding()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.backup.setOnClickListener {
+            MaterialAlertDialogBuilder(requireContext())
+                .setTitle("Backup")
+                .setMessage(
+                    "The backup file will be store in `/storage/emulated/0/Android/data/io.github.kedaitayar.mfm/files/backup/mfm_db-[DateTime].sqlite3`. " +
+                            "Move the file somewhere else because the file is deleted by Android after uninstalling the App." +
+                            " After the backup is done, the app will be restarted."
+                )
+                .setNegativeButton("Cancel") { dialog, _ ->
+                    dialog.cancel()
+                }
+                .setPositiveButton("Backup") { _, _ ->
+                    backup()
+                }
+                .show()
+        }
+
+        binding.restore.setOnClickListener {
+            restore()
+        }
+    }
+
+    private fun backup() {
+        RoomBackup()
+            .context(requireContext())
+            .database(MFMDatabase.getDatabase(requireContext()))
+            .enableLogDebug(false)
+            .backupIsEncrypted(false)
+            .useExternalStorage(true)
+            .apply {
+                onCompleteListener { success, message ->
+                    if (success) {
+                        restartApp(Intent(requireContext(), MainActivity::class.java))
+                    } else {
+                        mainViewModel.showSnackbar("There an error. Error message: $message", Snackbar.LENGTH_SHORT)
+                    }
+                }
+            }
+            .backup()
+    }
+
+    private fun restore() {
+        RoomBackup()
+            .context(requireContext())
+            .database(MFMDatabase.getDatabase(requireContext()))
+            .enableLogDebug(false)
+            .backupIsEncrypted(false)
+            .useExternalStorage(true)
+            .apply {
+                onCompleteListener { success, message ->
+                    if (success) {
+                        restartApp(Intent(requireContext(), MainActivity::class.java))
+                    } else {
+                        mainViewModel.showSnackbar("There an error. Error message: $message", Snackbar.LENGTH_SHORT)
+                    }
+                }
+            }
+            .restore()
+    }
 }
