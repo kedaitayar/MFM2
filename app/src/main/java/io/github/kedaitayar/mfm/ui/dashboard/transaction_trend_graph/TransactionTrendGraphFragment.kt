d9c038013f68a69ab1fa1ac0fd60c6cc8af2bbcc
kedaitayar <kedaitayar09@gmail.com>
2022-04-27 23:36:20 +0800

    fix: some flow migration

--- app/src/main/java/io/github/kedaitayar/mfm/ui/dashboard/transaction_trend_graph/TransactionTrendGraphFragment.kt
+++ app/src/main/java/io/github/kedaitayar/mfm/ui/dashboard/transaction_trend_graph/TransactionTrendGraphFragment.kt
@@ -2,26 +2,29 @@ package io.github.kedaitayar.mfm.ui.dashboard.transaction_trend_graph
 
 import android.os.Bundle
 import android.util.TypedValue
-import androidx.fragment.app.Fragment
 import android.view.View
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.github.mikephil.charting.charts.CombinedChart
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.formatter.ValueFormatter
 import dagger.hilt.android.AndroidEntryPoint
 import io.github.kedaitayar.mfm.R
 import io.github.kedaitayar.mfm.databinding.FragmentTransactionTrendGraphBinding
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class TransactionTrendGraphFragment : Fragment(R.layout.fragment_transaction_trend_graph) {
     private val transactionTrendGraphViewModel: TransactionTrendGraphViewModel by viewModels()
-    private var _binding: FragmentTransactionTrendGraphBinding? = null
-    private val binding get() = _binding!!
+    private val binding: FragmentTransactionTrendGraphBinding by viewBinding()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        _binding = FragmentTransactionTrendGraphBinding.bind(view)
 
         initViewModelColor()
         setupCombinedGraph()
@@ -30,13 +33,17 @@ class TransactionTrendGraphFragment : Fragment(R.layout.fragment_transaction_tre
     private fun initViewModelColor() {
         val typedValue = TypedValue()
         requireContext().theme.resolveAttribute(R.attr.gGreen, typedValue, true)
-        transactionTrendGraphViewModel.green = ContextCompat.getColor(requireContext(), typedValue.resourceId)
+        transactionTrendGraphViewModel.green =
+            ContextCompat.getColor(requireContext(), typedValue.resourceId)
         requireContext().theme.resolveAttribute(R.attr.gRed, typedValue, true)
-        transactionTrendGraphViewModel.red = ContextCompat.getColor(requireContext(), typedValue.resourceId)
+        transactionTrendGraphViewModel.red =
+            ContextCompat.getColor(requireContext(), typedValue.resourceId)
         requireContext().theme.resolveAttribute(R.attr.colorOnSurface, typedValue, true)
-        transactionTrendGraphViewModel.colorOnSurface = ContextCompat.getColor(requireContext(), typedValue.resourceId)
+        transactionTrendGraphViewModel.colorOnSurface =
+            ContextCompat.getColor(requireContext(), typedValue.resourceId)
         requireContext().theme.resolveAttribute(R.attr.colorPrimary, typedValue, true)
-        transactionTrendGraphViewModel.colorPrimary = ContextCompat.getColor(requireContext(), typedValue.resourceId)
+        transactionTrendGraphViewModel.colorPrimary =
+            ContextCompat.getColor(requireContext(), typedValue.resourceId)
     }
 
     private fun setupCombinedGraph() {
@@ -75,19 +82,16 @@ class TransactionTrendGraphFragment : Fragment(R.layout.fragment_transaction_tre
             axisRight.textColor = colorOnSurface
         }
 
-        transactionTrendGraphViewModel.transactionGraphCombinedData.observe(viewLifecycleOwner) {
-            it?.let {
-                binding.combinedChart.apply {
-                    data = it
-                    notifyDataSetChanged()
-                    invalidate()
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                transactionTrendGraphViewModel.transactionGraphCombinedData.collect {
+                    binding.combinedChart.apply {
+                        data = it
+                        notifyDataSetChanged()
+                        invalidate()
+                    }
                 }
             }
         }
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
--- app/src/main/java/io/github/kedaitayar/mfm/ui/dashboard/transaction_trend_graph/TransactionTrendGraphViewModel.kt
+++ app/src/main/java/io/github/kedaitayar/mfm/ui/dashboard/transaction_trend_graph/TransactionTrendGraphViewModel.kt
@@ -7,8 +7,7 @@ import com.github.mikephil.charting.data.*
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.github.kedaitayar.mfm.data.podata.TransactionGraphData
 import io.github.kedaitayar.mfm.data.repository.TransactionRepository
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 import kotlin.collections.set
@@ -19,14 +18,94 @@ class TransactionTrendGraphViewModel @Inject constructor(
 ) : ViewModel() {
     private val transactionYearlyTrendGraphFlow: Flow<List<TransactionGraphData>> =
         transactionRepository.getTransactionGraphData()
-    val transactionGraphCombinedData = MutableLiveData<CombinedData>()
+//    val transactionGraphCombinedData = MutableLiveData<CombinedData>()
+    val transactionGraphCombinedData = transactionYearlyTrendGraphFlow.map {
+        val barEntries = mutableListOf<BarEntry>()
+        val lineEntries = mutableListOf<Entry>()
+        val lineEntries2 = mutableListOf<Entry>()
+        val dataMapIncome = mutableMapOf<Int, TransactionGraphData>()
+        val dataMapExpense = mutableMapOf<Int, TransactionGraphData>()
+        var total: Double
+        for (item in it) {
+            when (item.transactionType) {
+                1 -> {
+                    dataMapExpense[item.transactionAgeInWeek] = item
+                }
+                2 -> {
+                    dataMapIncome[item.transactionAgeInWeek] = item
+                }
+            }
+        }
+        total = (dataMapIncome[-1]?.transactionAmountPrevYear
+            ?: 0.0) - (dataMapExpense[-1]?.transactionAmountPrevYear ?: 0.0)
+
+        val tempLineData = mutableListOf<Float>()
+
+        for (week in 0 until 54) {
+            val barEntry = BarEntry(
+                week.toFloat(),
+                floatArrayOf(
+                    dataMapIncome[week]?.transactionAmount?.toFloat() ?: 0f,
+                    -(dataMapExpense[week]?.transactionAmount?.toFloat() ?: 0f)
+                )
+            )
+            barEntries.add(barEntry)
+            total = total + (dataMapIncome[53 - week]?.transactionAmount
+                ?: 0.0) - (dataMapExpense[53 - week]?.transactionAmount ?: 0.0)
+            tempLineData.add(total.toFloat())
+        }
+
+        tempLineData.reversed().forEachIndexed { index, fl ->
+            lineEntries.add(Entry((index).toFloat(), fl))
+        }
+        val movingAverage = movingAverage(tempLineData, 12) { weightedMean() }
+
+        movingAverage.reversed().forEachIndexed { index, fl ->
+            lineEntries2.add(Entry((index).toFloat(), fl))
+        }
+
+        val barDataSet = BarDataSet(barEntries, "bardataset label")
+        barDataSet.colors = arrayListOf(
+            green,
+            red
+        )
+        barDataSet.setDrawValues(false)
+        val barData = BarData(barDataSet)
+
+        val lineDataSet = LineDataSet(lineEntries, "line label")
+        lineDataSet.apply {
+            setDrawValues(false)
+            mode = LineDataSet.Mode.HORIZONTAL_BEZIER
+            color = colorOnSurface
+            lineWidth = 1f
+            setDrawCircles(false)
+        }
+        val lineDataSet2 = LineDataSet(lineEntries2, "line label")
+        lineDataSet2.apply {
+            setDrawValues(false)
+            mode = LineDataSet.Mode.HORIZONTAL_BEZIER
+            color = colorPrimary
+            lineWidth = 1f
+            enableDashedLine(10f, 15f, 0f)
+            setDrawCircles(false)
+        }
+        val lineData = LineData(lineDataSet, lineDataSet2)
+
+        val combinedData = CombinedData().apply {
+            setData(barData)
+            setData(lineData)
+        }
+
+        combinedData
+    }
+
     var green = 0
     var red = 0
     var colorOnSurface = 0
     var colorPrimary = 0
 
     init {
-        setupTransactionGraphData()
+//        setupTransactionGraphData()
     }
 
     private fun setupTransactionGraphData() {
@@ -108,7 +187,7 @@ class TransactionTrendGraphViewModel @Inject constructor(
                     setData(lineData)
                 }
 
-                transactionGraphCombinedData.value = combinedData
+//                transactionGraphCombinedData.value = combinedData
             }
         }
     }
